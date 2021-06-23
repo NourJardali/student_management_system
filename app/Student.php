f9503a2c4ff4f26b4525fd6a0e5d5801e646f38a
nourhan.jardali@cadmusdatascience.com <^JMS|-#DVhc%>
2021-06-23 16:00:29 +0300

    student system management

---
+++ app/Student.php
@@ -0,0 +1,28 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model {
+    //
+    /**
+     * @var mixed
+     */
+    private $speciality;
+    /**
+     * @var mixed
+     */
+    private $age;
+    /**
+     * @var mixed
+     */
+    private $secondName;
+    /**
+     * @var mixed
+     */
+    private $firstName;
+    /**
+     * @var mixed
+     */
+    private $cne;
+}
